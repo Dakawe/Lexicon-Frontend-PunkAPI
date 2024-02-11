38343c3fc7ef139ece7699c1b6e47a814c7de444
Daniel Westergren <carl.dawe@gmail.com>
2024-02-11 17:04:13 +0100

    a beer-y start

---
+++ index.js
@@ -0,0 +1,26 @@
+let api = "https://api.punkapi.com/v2/beers";
+let beerlist = [];
+
+const fetcher = (addon = "") =>
+  fetch(api + addon)
+    .then((translate) => translate.json())
+    .then((beers) => beers)
+    .catch((error) => console.error(error));
+
+const randomize = (fetched = fetcher("/random")) =>
+  fetched.then((beer) =>
+    beer.forEach((self) => {
+      beerlist.push(self);
+      self.image_url ?? (self.image_url = "beer.png");
+      document.querySelector("#randombeer").innerHTML = `
+      <div style="background-color: #${~~((Math.random() * 899999) + 100000)}; background-image: url('${self.image_url}')"></div>
+      <h2>${self.name}</h2>
+      <b>More Info</b><br>
+      <b id="anotherBeer">Another Beer</b>`;
+      document.getElementById("anotherBeer").addEventListener("click", () => {
+        randomize();
+      });
+    })
+  );
+
+randomize();
