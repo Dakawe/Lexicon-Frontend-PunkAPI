eb4d0c0d00eb3ca81d903328f9fb5edb28227fcf
Daniel Westergren <carl.dawe@gmail.com>
2024-02-13 13:41:07 +0100

    not done

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-let api = "https://api.punkapi.com/v2/beers";
-let beerlist = [];
+const $ = (element = new String()) => document.querySelector(element),
+  output = [],
+  FetchAPI = async (url_addon = "", page, url = `https://api.punkapi.com/v2/beers?${url_addon}&page=${page}&per_page=9`) => {
+    return await (await fetch(url)).json();
+  },
+  StoreOutput = async (url, page, result) => {
+    result = await FetchAPI(url, page);
+    result.length && output.push(result) && (await StoreOutput(url, page + 1));
+  },
+  clearOldOutput = () => {
+    (output.length = 0), ($(`#results-pages`).innerHTML = ""), ($(`#results-list`).innerHTML = "");
+  };
 
-const fetcher = (addon = "") =>
-  fetch(api + addon)
-    .then((translate) => translate.json())
-    .then((beers) => beers)
-    .catch((error) => console.error(error));
-
-const randomize = (fetched = fetcher("/random")) =>
-  fetched.then((beer) =>
-    beer.forEach((self) => {
-      beerlist.push(self);
-      self.image_url ?? (self.image_url = "beer.png");
-      document.querySelector("#randombeer").innerHTML = `
-      <div style="background-color: #${~~(Math.random() * 899999 + 100000)}; background-image: url('${self.image_url}')"></div>
-      <h2>${self.name}</h2>
-      <b id="moreInfo">More Info</b><br>
-      <b id="anotherBeer">Another Beer</b>`;
-      document.getElementById("anotherBeer").addEventListener("click", () => randomize());
-      document.getElementById("moreInfo").addEventListener("click", () => randomize());
-    })
-  );
-
-randomize();
+$(`#search`).addEventListener(`submit`, async (e, input = new FormData(e.target), search) => {
+  e.preventDefault(), clearOldOutput();
+  search = `beer_name=${input.get("beer")}`;
+  await StoreOutput(search, 1);
+  for (const page in output) {
+    $(`#results-pages`).innerHTML += `<p class="page-select">${+page + 1}</p>`;
+  }
+  document.querySelectorAll(`.page-select`).forEach((page) =>
+    page.addEventListener(`click`, (p) => {
+      $(`#results-list`).innerHTML = "";
+      for (const self of output[+p.target.innerText - 1]) {
+        
+        $(`#results-list`).innerHTML += `<article><img src="${self.image_url ?? "beer.png"}"><h3>${self.name}</h3></article>`;
+      }
+    }));
+    $(".page-select").click();
+});
